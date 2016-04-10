38be416c1433323a05be30facd6b1c56f7039c69
Joaquin GALVAN <joar_12@hotmail.com>
2016-04-09 22:22:08 -0500

    Prueba con dashboard

---
+++ www/js/grayscale.js
@@ -0,0 +1,48 @@
+// jQuery to collapse the navbar on scroll
+holi();
+function holi (){
+  x = document.getElementsByTagName("body");
+  x = x[0];
+  x.setAttribute("data-spy","scroll");
+  x.setAttribute("data-target",".navbar-fixed-top");
+}
+/*document.getElementById('go').onclick = function() {
+  
+   document.styleSheets[2].disabled = true;
+};*/
+
+bd = document.getElementsByTagName("body");
+bd = bd[0];
+bd.className += " sidebar-collapse";
+  
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
+
+
+
